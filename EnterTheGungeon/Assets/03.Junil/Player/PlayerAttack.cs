d76ca8e740d14cbc995b70b8555705e11786adc2
DESKTOP-052EG82\KGA_22 <im5820795@naver.com>
2023-03-14 14:53:20 +0900

    [Junil] Add PlayerMarine Animater and Move, dodge

--- EnterTheGungeon/Assets/03.Junil/Player/PlayerAttack.cs
+++ EnterTheGungeon/Assets/03.Junil/Player/PlayerAttack.cs
@@ -6,18 +6,21 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private GameObject playerObj = default;
+    private GameObject rotateObjs = default;
     private Animator playerAni = default;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerObj = gameObject.transform.parent.gameObject;
-        playerAni = gameObject.transform.parent.gameObject.GetComponentMust<Animator>();
+        rotateObjs = gameObject.transform.parent.gameObject;
+        playerObj = rotateObjs.transform.parent.gameObject;
+        playerAni = rotateObjs.transform.parent.gameObject.GetComponentMust<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // { [Junil] 무기가 마우스 커서를 바라보는 코드
         /// @param Vector3 mousePos_ : 마우스 커서 위치 값
         Vector3 mousePos_ = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -33,56 +36,30 @@ public class PlayerAttack : MonoBehaviour
         
         transform.rotation = Quaternion.Euler(0, 0, rotateZ_);
 
-        GFunc.Log($"{lookZ_}");
-        if (-130f < lookZ_ && lookZ_ < 130f)
+        // } [Junil] 무기가 마우스 커서를 바라보는 코드
+
+        /// @param Vector2 rotateStand_ : 제대로 된 x 축 반전을 구하기 위한 값
+        Vector2 rotateStand_ = mousePos_ - rotateObjs.transform.position;
+
+
+        if(rotateStand_.x > 0)
         {
             gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
 
             playerObj.transform.localScale = new Vector3(1f, 1f, 1f);
         }
-        else if(-200f < lookZ_ && lookZ_ < -130f || 130f < lookZ_ && lookZ_ < 200f)
+        else
         {
-            /* Do Nothing */
-        }
-        else 
-        {
-
             gameObject.transform.localScale = new Vector3(-1f, -1f, 1f);
 
             playerObj.transform.localScale = new Vector3(-1f, 1f, 1f);
         }
 
+
         playerAni.SetFloat("inputX", mousePos_.x);
         playerAni.SetFloat("inputY", mousePos_.y);
 
 
-
-
-        // 캐릭터 애니메이션 작동
-        if (-180f < lookZ_ && lookZ_ <= -120f)
-        {
-            // 아래 보기
-        }
-        else if (-120f < lookZ_ && lookZ_ <= 40f)
-        {
-            // 오른쪽 보기
-        }
-        else if (40f < lookZ_ && lookZ_ <= 120f)
-        {
-            // 위 오른쪽 보기
-        }
-        else if (120f < lookZ_ && lookZ_ <= 180f)
-        {
-            // 위 보기
-        }
-        else if (180f < lookZ_ && lookZ_ <= 280f)
-        {
-            // 위 왼쪽 보기
-        }
-        else if (280f < lookZ_ || lookZ_ <= -180f )
-        {
-            // 왼쪽 보기
-        }
 
 
     }
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Project URP Change - Built in to URP.
 [HT]
 총알을 발사 할 때, RayCast가 총알의 Layer에 막혀 로직에 문제가 발생
 Raycast 에서 Bullet의 LayerMask를 제외하고 Hit 체크를 하도록 설정
+
+
+[Junil]    
+2023/03/14 / v0.0.1 / Add PlayerMarine Animater and Move, dodge
