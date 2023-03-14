bb681cbd8b95c19a0c03dbbcbae0cf4674740248
DESKTOP-052EG82\KGA_22 <im5820795@naver.com>
2023-03-14 10:30:11 +0900

    [Junil] fix code

--- EnterTheGungeon/Assets/03.Junil/MonsterBullets.cs
+++ EnterTheGungeon/Assets/03.Junil/MonsterBullets.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class MonsterBullets : MonoBehaviour
 {
     // { [Junil] 몬스터 총알은 이 스크립트를 상속받기
-    private void OnEnable()
+    public virtual void OnEnable()
     {
         PlayerController.OnPlayerBlankBullet += this.OnPlayerBlankBullet;
     }
 
-    private void OnDisable()
+    public virtual void OnDisable()
     {
         PlayerController.OnPlayerBlankBullet -= this.OnPlayerBlankBullet;
 
     }
 
     /// @brief 공포탄 작동 시 적 총알 오브젝트가 꺼지는 함수
-    void OnPlayerBlankBullet()
+    public virtual void OnPlayerBlankBullet()
     {
         gameObject.SetActive(false);
     }
