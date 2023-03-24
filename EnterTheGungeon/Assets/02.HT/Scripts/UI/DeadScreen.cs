cfc7d215425a3f5249efe630fb094664de92a6ef
GX1018 <wjdghlxo@gmail.com>
2023-03-24 17:43:17 +0900

    [HT]make DeadScreen, shotClockHair, active ammonomicon

--- EnterTheGungeon/Assets/02.HT/Scripts/UI/DeadScreen.cs
+++ EnterTheGungeon/Assets/02.HT/Scripts/UI/DeadScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,15 +23,34 @@ public class DeadScreen : MonoBehaviour
 
     float upsideHeight;
     float downsideHeight;
-    bool isSideSizeCheck;
+    public bool isSideSizeCheck;
     // } Var for side
 
+    // { Var for ClockHair
+    Transform player;
+    Transform clockHair;
+    RectTransform clockHairRectTransform;
+    Image clockHairImage;
+    Image hourHandImage;
+    Image minuteHandImage;
+    Image secondHandImage;
+    RectTransform hourHandRectTransform;
+    RectTransform minuteHandRectTransform;
+    RectTransform secondHandRectTransform;
+    // } Var for ClockHair
+
+    public bool isShotEnd;
 
 
     public bool isTest;
+    GameObject gamePause;
+    GameObject ammonomicon;
     // Start is called before the first frame update
     void Start()
     {
+        gamePause = transform.parent.GetChild(2).gameObject;
+        ammonomicon = gamePause.transform.GetChild(1).gameObject;
+
         background = transform.GetChild(0);
         background.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
         chageColorSpeed = 1;
@@ -38,11 +58,31 @@ public class DeadScreen : MonoBehaviour
         upside = transform.GetChild(0).GetChild(0).GetComponent<RectTransform>();
         downside = transform.GetChild(0).GetChild(1).GetComponent<RectTransform>();
         downside.sizeDelta = Vector2.zero;
+
+        //{ animation
+        clockHair = transform.GetChild(1);
+
+        clockHairRectTransform = clockHair.GetComponent<RectTransform>();
+        clockHairImage = clockHair.GetComponent<Image>();
+        player = GameObject.FindWithTag("Player").transform;
+
+
+        hourHandImage = clockHair.GetChild(1).GetComponent<Image>();
+        hourHandRectTransform = clockHair.GetChild(1).GetComponent<RectTransform>();
+        minuteHandImage = clockHair.GetChild(0).GetComponent<Image>();
+        minuteHandRectTransform = clockHair.GetChild(0).GetComponent<RectTransform>();
+        secondHandImage = clockHair.GetChild(2).GetComponent<Image>();
+        secondHandRectTransform = clockHair.GetChild(2).GetComponent<RectTransform>();
+
+        //} animation
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        ImageSizeSet();
+
         if (!isColorChange)
         {
             backgroundColorRValue = background.GetComponent<Image>().color.r;
@@ -71,9 +111,6 @@ public class DeadScreen : MonoBehaviour
             downside.sizeDelta = new Vector2(0, downsideHeight);
 
 
-            //upside.localPosition = new Vector3(0, -upsideHeight / 2f, 0);
-            //downside.localPosition = new Vector3(0, downsideHeight / 2f, 0);
-
             if (upsideHeight >= 214)
             {
                 isSideSizeCheck = true;
@@ -87,7 +124,37 @@ public class DeadScreen : MonoBehaviour
             downside.sizeDelta = new Vector2(0, 0);
             isColorChange = false;
             isSideSizeCheck = false;
+
+
             isTest = false;
         }
+
+        if (isShotEnd)
+        {
+            StartCoroutine(LoadAmmonomicon());
+        }
+    }
+
+
+    void ImageSizeSet()
+    {
+        clockHairImage.SetNativeSize();
+        hourHandImage.SetNativeSize();
+        minuteHandImage.SetNativeSize();
+        secondHandImage.SetNativeSize();
+
+        clockHairRectTransform.sizeDelta = new Vector2(clockHairRectTransform.sizeDelta.x * 3, clockHairRectTransform.sizeDelta.y * 3);
+        hourHandRectTransform.sizeDelta = new Vector2(hourHandRectTransform.sizeDelta.x * 3, hourHandRectTransform.sizeDelta.y * 3);
+        minuteHandRectTransform.sizeDelta = new Vector2(minuteHandRectTransform.sizeDelta.x * 3, minuteHandRectTransform.sizeDelta.y * 3);
+        secondHandRectTransform.sizeDelta = new Vector2(secondHandRectTransform.sizeDelta.x * 3, secondHandRectTransform.sizeDelta.y * 3);
+    }
+
+    IEnumerator LoadAmmonomicon()
+    {
+        isShotEnd = false;
+        yield return new WaitForSeconds(3);
+        gamePause.SetActive(true);
+        yield return null;
+        ammonomicon.SetActive(true);
     }
 }
